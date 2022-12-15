89e00a715886f1aa25d15b2c0f3d8dbbfa6fbbb1
user_sunshine <ian.n.sorensen@gmail.com>
2022-12-14 17:19:47 -0700

    stripe technologies implemented on cart

--- api/cart.js
+++ api/cart.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { getActiveCartByUserId, purchaseCart, getActiveCartId} = require('../db/cart');
 const cartRouter = express.Router();
-const {requireUser} = require('./utils');
+const {requireUser, makePurchase} = require('./utils');
 const {getAllCartsUserId, addItemToCartDetails, addQuantityToCart,
        removeItemFromCartDetails } = require('../db/cartDetails');
 
@@ -60,12 +60,31 @@ cartRouter.patch('/',requireUser, async(req,res,next)=>{
 //PATCH /api/cart/purchase (sets cart to false, assigns bought at value)
 cartRouter.patch('/purchase',requireUser,async(req,res,next)=>{
     try{
-        const {cartId}= await getActiveCartByUserId(req.user.id)
+        const {cartId,totalPrice}= await getActiveCartByUserId(req.user.id)
+        req.totalPrice = totalPrice;
         const purchase = await purchaseCart(cartId,req.user.id);
-        console.log("Purchase made, enjoy your posters!")
-        res.send( {name:"Purchase successful.",message:"Enjoy your posters! Thank you for shopping with Poster-Children!",purchase:purchase})
+        if(purchase) {
+            console.log("Purchase made, enjoy your posters!")
+            res.send( {name:"Purchase successful.",message:"Enjoy your posters! Thank you for shopping with Poster-Children!",purchase:purchase})
+        }else{
+            res.send({name:"Purchase Unsuccessful", message:"no posters for you"})
+        }
     }catch(error){
-        next({name:"Failure to purchase",message:"An error occurred while making your purchase, please contact us or try again."})
+        console.log(error)
+        res.send({name:"Failure to purchase",message:"An error occurred while making your purchase, please contact us or try again."})
+    }
+})
+
+cartRouter.post('/paymentIntent',requireUser,async(req,res,next)=>{
+    try{
+        const {totalPrice} = await getActiveCartByUserId(req.user.id)
+    req.totalPrice = totalPrice;
+    req.clientSecret = await makePurchase(req,res);
+    if(!req.clientSecret) res.send({name:"no secret", message:"nothing was in your pocketses"})
+    else res.send( {clientSecret: req.clientSecret, name: "got client secret (mulk)"})
+    }catch(error){
+        console.log(error);
+        res.send(error);
     }
 })
 
